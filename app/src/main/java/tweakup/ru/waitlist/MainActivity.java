c2f6d181b1a12a31d8f338477e982d94f6bcabfa
MFALLAM <mohammed.fathi.allam@gmail.com>
2023-11-29 12:33:50 +0300

    Initial app start

---
+++ app/src/main/java/tweakup/ru/waitlist/MainActivity.java
@@ -0,0 +1,14 @@
+package tweakup.ru.waitlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
