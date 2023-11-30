1a5a063a5fb85dcb8025f35b8ca345a09f87b386
MFALLAM <mohammed.fathi.allam@gmail.com>
2023-11-30 14:56:43 +0300

    updated guestlistadapter

--- app/src/main/java/tweakup/ru/waitlist/MainActivity.java
+++ app/src/main/java/tweakup/ru/waitlist/MainActivity.java
@@ -1,9 +1,13 @@
 package tweakup.ru.waitlist;
 
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -22,10 +26,18 @@ public class MainActivity extends AppCompatActivity {
     // Local field for database
     private SQLiteDatabase mDd;
 
+    private EditText mNewGuestNameEditText;
+    private EditText mNewPartySizeEditText;
+
+    private final static String LOG_TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mNewGuestNameEditText = findViewById(R.id.person_name_edit_text);
+        mNewPartySizeEditText = findViewById(R.id.party_count_edit_text);
 
         RecyclerView waitlistRecyclerView;
 
@@ -59,7 +71,35 @@ public class MainActivity extends AppCompatActivity {
      * @param view The calling view (button)
      */
     public void addToWaitlist(View view) {
+        // TODO first thing, check if any of the EditTexts are empty, return if so
+        if (mNewGuestNameEditText.getText().length() == 0 ||
+                mNewPartySizeEditText.getText().length() == 0) {
+            return;
+        }
 
+        // Initialize partySize to 1
+        int partySize = 1;
+
+        // TODO Use Integer.parseInt to parse mNewPartySizeEditText to an integer
+        // TODO Make sure you surround the Integer.parseInt with try ctach and log any exception
+        try {
+            partySize = Integer.parseInt(mNewPartySizeEditText.getText().toString());
+        } catch (Exception e) {
+            Toast.makeText(this, "Something went wrong..!", Toast.LENGTH_LONG).show();
+        }
+
+        // TODO call addNewGuest with the guest name and party size
+        addNewGuest(mNewGuestNameEditText.getText().toString(), partySize);
+
+        // TODO call mAdapter.swapCusror to update the cursor by passing in getAllGuests()
+        mAdapter.swapCursor(getAllGuests());
+
+        // TODO make the UI look nice, call .getText().clear() on both EditTexts, also call clearFocus() on mNewPartSize
+        // COMPLETED (20) To make the UI look nice, call .getText().clear() on both EditTexts, also call clearFocus() on mNewPartySizeEditText
+        //clear UI text fields
+        mNewPartySizeEditText.clearFocus();
+        mNewGuestNameEditText.getText().clear();
+        mNewPartySizeEditText.getText().clear();
     }
 
     // TODO (5) Create a private method called getAllGuests that returns a cursor
@@ -76,4 +116,18 @@ public class MainActivity extends AppCompatActivity {
         );
     }
 
+    private long addNewGuest(String name, int partySize) {
+        // TODO create a ContentValues instance to pass the value onto the insert query
+        ContentValues contentValues = new ContentValues();
+
+        // TODO call the put to insert name value with they key COLUMN_GUEST_NAME
+        contentValues.put(WaitListContract.WaitlistEntry.COLUMN_GUEST_NAME, name);
+
+        // TODO call the put to insert party size value with the key COLUMN_PARTY_SIZE
+        contentValues.put(WaitListContract.WaitlistEntry.COLUMN_PARTY_SIZE, partySize);
+
+        long operationId = mDd.insert(WaitListContract.WaitlistEntry.TABLE_NAME, null, contentValues);
+        Log.d(LOG_TAG, "Opeartion ID_ " + operationId);
+        return operationId;
+    }
 }
